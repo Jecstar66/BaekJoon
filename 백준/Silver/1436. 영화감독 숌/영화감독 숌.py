6016aae1fb491bd5fe9a710f4749496cf700322e
Jecstar66 <79183682+Jecstar66@users.noreply.github.com>
2023-07-03 21:28:30 +0900

    [Silver V] Title: 영화감독 숌, Time: 796 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,12 @@
+N = int(input())
+rank = 1
+num = 666
+if N == 1 :
+    print(num)
+else :
+    while(rank != N):
+        num = num + 1
+        if "666" in str(num):
+            rank = rank + 1
+    print(num)
+        
