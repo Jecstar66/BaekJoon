06fa4271fdf4b37e4ee97ff30ef65443241f7097
Jecstar66 <79183682+Jecstar66@users.noreply.github.com>
2023-07-04 12:36:11 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.py
@@ -0,0 +1,8 @@
+## Use map(int, input().split()) for integer input allocation.
+## Reference : https://dev-mandos.tistory.com/124
+import math
+import sys
+
+n = int(input())
+print((n*(n-1)*(n-2))//6)
+print(3)
