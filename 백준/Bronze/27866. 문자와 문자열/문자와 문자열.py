1c6cff081cf9a3a058bd20ab6d9fce3ada9b0275
Jecstar66 <79183682+Jecstar66@users.noreply.github.com>
2023-07-04 14:56:14 +0900

    [Bronze V] Title: 문자와 문자열, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.py
@@ -0,0 +1,8 @@
+## Use map(int, input().split()) for integer input allocation.
+## Reference : https://dev-mandos.tistory.com/124
+import math
+import sys
+
+S = input()
+i = int(input())
+print(S[i-1])
