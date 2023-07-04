82951625ef4587e9ed57eedcd24b63e2d27d151b
Jecstar66 <79183682+Jecstar66@users.noreply.github.com>
2023-07-04 21:23:35 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 52 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -0,0 +1,11 @@
+## Use map(int, input().split()) for integer input allocation.
+## Reference : https://dev-mandos.tistory.com/124
+import math
+import sys
+
+while True:
+    try:
+        rl = input()
+        print(rl)
+    except EOFError:
+        break
