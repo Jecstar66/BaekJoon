2a9e278d145cf9f0bb399edda392613301748915
Jecstar66 <79183682+Jecstar66@users.noreply.github.com>
2023-07-04 20:36:18 +0900

    [Bronze II] Title: 단어의 개수, Time: 56 ms, Memory: 41476 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -0,0 +1,10 @@
+## Use map(int, input().split()) for integer input allocation.
+## Reference : https://dev-mandos.tistory.com/124
+import math
+import sys
+
+sent = input()
+sent.strip()
+arr = sent.split()
+print(len(arr))
+        
