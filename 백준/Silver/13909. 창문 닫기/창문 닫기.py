5e119586573f35ceeee7de293efea5c1525db52f
Jecstar66 <79183682+Jecstar66@users.noreply.github.com>
2023-07-11 03:46:11 +0900

    [Silver V] Title: 창문 닫기, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.py
@@ -0,0 +1,8 @@
+## Use map(int, input().split()) for integer input allocation.
+## Commentation : Alt + Shift + A
+## Advantage for Sieve Alg. : when [a range of number] is input
+import math
+
+n = int(input())
+
+print(int(math.sqrt(n)))
